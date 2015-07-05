28a92d9b8bc8e9f42ac6f13bc73d83a3df38a8fe
Jian Zhen <zhen0000@gmail.com>
2015-07-05 23:58:13 +0000

    modified index.js and index.html

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var express = require('express')
+var express = require('express');
+var fs = require('fs');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  var buf = fs.readFileSync("index.html");
+  response.send(buf.toString('ascii'));
 })
 
 app.listen(app.get('port'), function() {
